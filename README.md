69a1f0e1f363ff9c646b3713a9f79fbe0ded1449
David <pql@cock.li>
2018-10-21 17:20:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# pwntools-templater
+
+A small templating script to quickly generate pwntools scripts for binary exploitation CTF challenges.
+I made this tool to assist in some CTF competitions, and I hope it will come in handy for you as well.
+
+## Synopsis
+
+```
+usage: template.py [-h] [--local LOCAL] [--remote REMOTE]
+                   [--env ENV [ENV ...]] [--elfs ELFS [ELFS ...]]
+                   [--outfile [OUTFILE]]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --local LOCAL, -l LOCAL
+                        The local executable path.
+  --remote REMOTE, -r REMOTE
+                        The remote host and port to connect to. (host:port)
+  --env ENV [ENV ...]   The environment to be passed to the local executable.
+                        (supplied like "KEY=VALUE")
+  --elfs ELFS [ELFS ...], -e ELFS [ELFS ...]
+                        The ELF files to be accessed in the script. (via
+                        pwntools' ELF())
+  --outfile [OUTFILE]   The file to write the script to. ('-' by default)
+```
+
+## Installation
+```
+git clone https://github.com/pql-2000/pwntools-templater
+```
---
+++ template.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+import sys
+import argparse
+import os
+
+if sys.version_info[0] < 3 or sys.version_info[1] < 6:
+    sys.stderr.write("Python 3.6+ is required for this program to run. (fstrings)\n")
+    exit(1)
+
+def generate_template(args):
+    # DO NOT JUDGE
+
+    env = ('{' + ', '.join(':'.join(["'" + y + "'" for y in x.split('=')]) for x in args.env) + '}') if args.env else 'os.environ'
+    exec_path = args.local if args.local else ''
+    remote_host, remote_port = args.remote.split(':') if args.remote else ('', '')
+    elfs = ''.join([f'ELF_{i} = ELF("{elf}")'+'\n' for i, elf in enumerate(args.elfs)]) if args.elfs else ''
+
+    return \
+    f"""\
+from pwn import *
+import sys
+import os
+
+if sys.version_info[0] == 2:
+    input = raw_input
+
+def handle():
+    env = {env}
+    
+    if args.LOCAL:
+        return process('{exec_path}', env=env)
+    elif args.GDB:
+        return gdb.debug('{exec_path}', env=env)
+    elif args.REMOTE:
+        return remote('{remote_host}', {remote_port})
+    print('Please supply an option via command line arguments.')
+    exit(1)
+
+{elfs}
+
+def main():
+    l = handle()
+    if args.GDB:
+        input()
+
+    '''
+    Your code here
+    '''
+    """
+        
+
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--local', '-l', help="The local executable path.")
+    parser.add_argument('--remote', '-r', help="The remote host and port to connect to. (host:port)")
+    parser.add_argument('--env', nargs='+', help="The environment to be passed to the local executable. (supplied like \"KEY=VALUE\")")
+    parser.add_argument('--elfs', '-e', nargs='+', help="The ELF files to be accessed in the script. (via pwntools' ELF())")
+
+    parser.add_argument('--outfile', nargs='?', help="The file to write the script to. ('-' by default)", default='-')
+    args = parser.parse_args()
+    
+    if args.outfile == '-':
+        out = sys.stdout
+    else:
+        if os.path.exists(args.outfile):
+            print("*****Writing script terminated: File already exists*****", file=sys.stderr)
+            print("Writing to stdout instead....", file=sys.stderr)
+            print("-"*80, file=sys.stderr)
+            out = sys.stdout
+        else:
+            out = open(args.outfile, 'w')
+
+
+    print(generate_template(args), file=out)
+
+if __name__ == "__main__":
+    main()
